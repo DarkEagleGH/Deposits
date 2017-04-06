9784fd3072701435897fdaef76a45fb15a46f76e
vshatokhin <v.shatokhin@pfsoft.org>
2017-04-06 16:43:45 +0300

    deleted comment, added new logic -> InputValidation

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,5 +1,35 @@
 package client;
 
-public class Client {
+import java.util.Scanner;
 
+public class Client {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String enter = scanner.nextLine();
+        validationEnter(enter);
+    }
+
+    static boolean validationEnter(String enter) {
+        boolean result = false;
+        //TODO
+        String operation[] = {"list", "sum", "count"};
+        try {
+            for (int i = 0; i < enter.length(); i++) {
+                if (enter.equals(operation[i])) {
+                    result = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+        }
+        if (!result) {
+            System.out.println("Вы ввели недопустимую команду, воспользуйтесь командой \"help\" чтобы вывести подсказку");
+        }
+        return result;
+    }
+
+    static void request(String enter) {
+
+    }
 }
