87c1c8fe21038ab432fb7f9a184050a2443ccc10
kpeatiBwik <dp100991svi@gmail.com>
2017-04-09 00:28:39 +0300

    added new bugs :)

--- src/main/java/Helpers/FileReaderForHelp.java
+++ src/main/java/Helpers/FileReaderForHelp.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 public class FileReaderForHelp {
 
     public static void ReadHelp() {
-//        try (FileReader fileReader = new FileReader("src/main/resources/help.txt")) {
         try (FileReader fileReader = new FileReader("help.txt")) {
             int c;
             while ((c = fileReader.read()) != -1) System.out.print((char) c);
