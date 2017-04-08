c7dd3b05623cf4d784e730003aff7eaca13265ee
kpeatiBwik <dp100991svi@gmail.com>
2017-04-08 23:07:15 +0300

    added class WriteResponse

---
+++ src/main/java/Helpers/WriteResponse.java
@@ -0,0 +1,25 @@
+package Helpers;
+
+public class WriteResponse {
+
+    public String writeResponse(String command, String data) {
+        String result = null;
+        switch (command) {
+            case "list":
+                result = data.replaceAll(";", "\n");
+                break;
+            case "sum":
+            case "count":
+                break;
+            case "info account":
+            case "info depositor":
+            case "show type":
+            case "show bank":
+            case "add":
+            case "delete":
+                break;
+        }
+        return result;
+    }
+
+}
